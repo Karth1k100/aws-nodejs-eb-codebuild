519f222965ef02013cd06319ce06d4ce94a80f08
luis.gonzalez.tirado87@gmail.com <luis.gonzalez.tirado87@gmail.com>
2019-03-12 08:48:34 -0400

    commit

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({extended:false}))
 
 app.get('/', function (req, res) {
   res.render('index', {
-    title: 'BackSpace Academy & Elastic Beanstalk'
+    title: 'This is a test'
     })
     res.status(200).end();
 })
