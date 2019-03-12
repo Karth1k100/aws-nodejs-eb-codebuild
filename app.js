777b9ebc2b5003db0a90a9f5d1e6b8cd5f4a1f0f
luisgonzalez1 <39278497+luisgonzalez1@users.noreply.github.com>
2019-03-12 09:10:29 -0700

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var AWS = require('aws-sdk')
 
 var express = require('express')
 var bodyParser = require('body-parser')
-var movieRouter =require('./routers/movie-router');
-var userRouter = require('./routers/user-router');
+var movieRouter =require('./routers/movie-router.js');
+var userRouter = require('./routers/user-router.js');
 
 // Set region for AWS SDKs
 AWS.config.region = process.env.REGION
