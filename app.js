c3320db9ba1041e91d06507c1cc1580a39b8daee
luisgonzalez1 <39278497+luisgonzalez1@users.noreply.github.com>
2019-03-12 13:46:00 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 var AWS = require('aws-sdk')
 
 var express = require('express')
-var bodyParser = require('body-parser')
-var movieRouter = require('./routers/movie-router.js');
+//var bodyParser = require('body-parser')
+//var movieRouter = require('./routers/movie-router.js');
 //var userRouter = require('./routers/user-router.js');
 //import  movieRouter  from './routers/movie-router.js';
 //import  userRouter  from './routers/user-router.js';
@@ -28,7 +28,7 @@ app.get('/test-endpoint', (req, res) => {
   res.json({content: 'hello Karthik'});
 });
 
-app.use('/movies', movieRouter);
+//app.use('/movies', movieRouter);
 // app.use('/users', userRouter);
 var port = process.env.PORT || 3000
 
